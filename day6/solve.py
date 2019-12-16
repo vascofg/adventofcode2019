e512b523509ba3b69e75855ac3bbfe5aefaf3359
Vasco Gonçalves <vascofg@gmail.com>
2019-12-16 23:55:03 +0100

    add day6 part2

--- day6/solve.py
+++ day6/solve.py
@@ -6,20 +6,45 @@ def makeGraph(orbits):
         graph[A].append(B)
     return graph
 
-def dfs(node, graph, previousEdges = 0):
+
+def ocbSolve(node, graph, previousEdges=0):
     sum = 0
     if(node in graph):
         children = graph[node]
 
         for child in children:
-            # for each branch, go until the end and sum the number of previous edges
-            sum = sum + dfs(child, graph, previousEdges + 1)
+            # for each branch, go until the end
+            sum = sum + ocbSolve(child, graph, previousEdges + 1)
 
+    # count the number of previous edges
     return sum + previousEdges
 
+
 def ocb(orbits):
-    return dfs("COM", makeGraph(orbits))
-        
+    return ocbSolve("COM", makeGraph(orbits))
+
+
+def orbitalTransfers(orbits):
+    invertedGraph = makeGraph([(B, A) for A, B in orbits])
+    origin = invertedGraph["YOU"][0]
+    destination = invertedGraph["SAN"][0]
+    originToCOM = getVisited(invertedGraph, origin, "COM", [])
+    destinationToCOM = getVisited(invertedGraph, destination, "COM", [])
+    commonAncestor = firstCommonAncestor(originToCOM, destinationToCOM)
+    return originToCOM.index(commonAncestor) + destinationToCOM.index(commonAncestor)
+
+
+def getVisited(graph, node, destination, stack):
+    # get all visited nodes to get to COM
+    stack.append(node)
+    if(node == destination):
+        return stack
+    else:
+        return getVisited(graph, graph[node][0], destination, stack)
+
+
+def firstCommonAncestor(visited1, visited2):
+    return next(node for node in visited1 if node in visited2)
 
 
 if __name__ == '__main__':
@@ -27,4 +52,4 @@ if __name__ == '__main__':
         orbits = [tuple(line.strip().split(")")) for line in file]
     print(ocb(orbits))
 
-    
+    print(orbitalTransfers(orbits))
--- day6/test.py
+++ day6/test.py
@@ -19,3 +19,21 @@ class TestOCB(unittest.TestCase):
         ]
         self.assertEqual(ocb(orbits), 42)
 
+class TestOrbitalTransfers(unittest.TestCase):
+    def test_orbitalTransfers(self):
+        orbits = [
+            ("COM", "B"),
+            ("B", "C"),
+            ("C", "D"),
+            ("D", "E"),
+            ("E", "F"),
+            ("B", "G"),
+            ("G", "H"),
+            ("D", "I"),
+            ("E", "J"),
+            ("J", "K"),
+            ("K", "L"),
+            ("K", "YOU"),
+            ("I", "SAN")
+        ]
+        self.assertEqual(orbitalTransfers(orbits), 4)
